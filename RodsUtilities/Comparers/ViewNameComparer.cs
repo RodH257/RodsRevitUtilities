b354e9ae635eddbf6a0c4a7a56e32cf8e84dab90
Rodh257 <rodhowarth257@gmail.com>
2011-03-31 10:01:56 +1000

    added comparers for views to allow for easy sorting

---
+++ RodsUtilities/Comparers/ViewNameComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.Revit.DB;
+
+namespace RodsUtilities.Comparers
+{
+    /// <summary>
+    /// Comparer for Views by View Name
+    /// </summary>
+    public class ViewNameComparer: IComparer<View>
+    {
+        public int Compare(View x, View y)
+        {
+            return x.ViewName.CompareTo(y.ViewName);
+        }
+    }
+}
---
+++ RodsUtilities/Comparers/ViewSheetNumberComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.Revit.DB;
+
+namespace RodsUtilities.Comparers
+{
+    /// <summary>
+    /// Compares view sheets by sheet number
+    /// </summary>
+    public class ViewSheetNumberComparer: IComparer<ViewSheet>
+    {
+        public int Compare(ViewSheet x, ViewSheet y)
+        {
+            return x.SheetNumber.CompareTo(y.SheetNumber);
+        }
+    }
+}
